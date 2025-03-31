f5ea65eea5f8f424968fcefa4c5fa2e583e461b7
mananuf <bmananaf@gmail.com>
2025-03-31 16:18:43 +0100

    adds dynamic traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 #[derive(Debug)]
 struct Book {
     title: String,
@@ -19,6 +21,20 @@ impl Book {
     }
 }
 
+impl Readable for Book {
+    fn title(&self) -> &str {
+        &self.title
+    }
+
+    fn author(&self) -> &str {
+        &self.author
+    }
+
+    fn publication_year(&self) -> u16 {
+        2025
+    }
+}
+
 #[derive(Debug)]
 enum BookStatus {
     Available,
@@ -29,7 +45,52 @@ enum BookStatus {
 
 #[derive(Debug)]
 struct Libary {
-    books: Vec<Book>,
+    books: Vec<Box<dyn Readable>>,
+}
+
+trait Readable: Debug {
+    fn title(&self) -> &str;
+    fn author(&self) -> &str;
+    fn publication_year(&self) -> u16;
+    fn summary(&self) -> String {
+        format!(
+            "{} by {}, published in {}",
+            self.title(),
+            self.author(),
+            self.publication_year()
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Magazine {
+    title: String,
+    author: String,
+    publication_date: u16,
+}
+
+impl Magazine {
+    fn new(title: String, author: String, publication_date: u16) -> Self {
+        Self {
+            title,
+            author,
+            publication_date,
+        }
+    }
+}
+
+impl Readable for Magazine {
+    fn author(&self) -> &str {
+        &self.author
+    }
+
+    fn publication_year(&self) -> u16 {
+        self.publication_date
+    }
+
+    fn title(&self) -> &str {
+        &self.title
+    }
 }
 
 impl Libary {
@@ -37,22 +98,31 @@ impl Libary {
         Self { books: Vec::new() }
     }
 
-    fn add_book(&mut self, book: Book) -> () {
-        self.books.push(book)
+    fn add_to_collection<T: Readable + 'static>(&mut self, item: T) -> () {
+        self.books.push(Box::new(item))
     }
 
     fn checkout(&mut self, title: String) -> () {
-        if let Some(book) = self.books.iter_mut().find(|book| book.title == title) {
-            book.status = BookStatus::CheckedOut;
-            println!("Book Status updated for {}", book.title);
+        if let Some(book) = self.books.iter_mut().find(|book| book.title() == title) {
+            // book.status = BookStatus::CheckedOut;
+            println!("Book Status updated for {}", book.title());
         } else {
             println!("Book does not exists");
         }
     }
 
-    fn all_books(&self) -> &Vec<Book> {
+    fn all_books(&self) -> &Vec<Box<dyn Readable + 'static>> {
         &self.books
     }
+}
+
+fn print_details<T: Readable>(item: &T) -> String {
+    format!(
+        "TITLE: {} \nAUTHOR: {} \nYEAR: {}",
+        item.title(),
+        item.author(),
+        item.publication_year()
+    )
 }
 
 fn main() {
@@ -70,10 +140,19 @@ fn main() {
         String::from("10th april 2008"),
     );
 
+    let forbes: Magazine = Magazine::new(
+        "forbes Magazine".to_string(),
+        "The Grammys".to_string(),
+        2020,
+    );
+
     let mut library: Libary = Libary::new();
 
-    library.add_book(things_fall_apart);
-    library.add_book(purple_hibiscus);
+    print_details(&purple_hibiscus);
+
+    library.add_to_collection(things_fall_apart);
+    library.add_to_collection(purple_hibiscus);
+    library.add_to_collection(forbes);
 
     dbg!(library.all_books());
 
